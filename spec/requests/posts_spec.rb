12056ae76129fd012519a65bec168fcbab6580bc
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-08-22 19:48:21 +0500

    Add index and show actions for User and Post controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,42 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe 'For the Post model' do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:all) do
+      get user_posts_path(user_id: 1)
+    end
+
+    it 'returns http success' do
+      get '/users/show/posts/'
+      puts response
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
     before(:each) do
-      @user = User.new(name: 'Tom', photo: 'image.png', bio: 'Teacher from Mexico', post_counter: 0)
-      @post = Post.new(author: @user, title: 'Test', text: 'testing', likes_counter: 7, comments_counter: 5)
+      get user_post_path(user_id: 1, id: 1)
     end
 
-    before { @post.save }
-
-    it 'if there is title' do
-      @post.title = true
-      expect(@post).to be_valid
+    it 'returns http success' do
+      get '/users/show/posts/show'
+      expect(response).to have_http_status(:success)
     end
 
-    it 'if there is max 250 characters' do
-      @post.title = 'Testing'
-      expect(@post).to be_valid
+    it 'returns http success' do
+      expect(response).to render_template(:show)
     end
 
-    it 'if likes counter is integer' do
-      @post.likes_counter = 5
-      expect(@post).to be_valid
-    end
-
-    it 'if likes counter greater than or equal to zero' do
-      @post.likes_counter = -9
-      expect(@post).to_not be_valid
-    end
-
-    it 'if comments counter greater than or equal to zero.' do
-      @post.comments_counter = -5
-      expect(@post).to_not be_valid
-    end
-
-    it 'if comments counter is integer' do
-      @post.comments_counter = 8
-      expect(@post).to be_valid
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a specific post for specific user')
     end
   end
 end
